1c5ace2be682734118bf7df99b8566d710e53536
Harsh Shandilya <me@msfjarvis.dev>
2023-03-28 15:56:55 +0530

    gradle: build.gradle -> build.gradle.kts
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+// Remove when switching to Gradle 8.1
+// https://github.com/gradle/gradle/issues/22797
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -12,4 +15,4 @@ tasks {
     }
 }
 
-apply from: "version.gradle"
+apply(from = "version.gradle")
